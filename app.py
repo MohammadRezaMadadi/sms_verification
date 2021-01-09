0c4544f8f210221a366bf96bd2bbafeb4c82900c
MohammadRezaMdadi <madadimr@yahoo.com>
2021-01-09 16:30:38 +0000

    upgreade recive function with something more

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.rout('/Ver1/recive_sms', methods=[post])
+def recive_sms():
+    '''function to recive customer's message'''
+    data = request.form
+    sndr = data['from']
+    msg = data['message']
+    print(f'We recived: {msg} from: {sndr}')
+    return jsonify(msg), 200
+
+def send_sms():
+    '''function to send verification to customer'''
+    pass
+
+def chk_serial():
+    '''function to check serial number sent from customer'''
+    pass
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',5000, debug= True)
+
+
